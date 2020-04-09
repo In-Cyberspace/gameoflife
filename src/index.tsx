cea20789808628132bcef79e312c3c679bf096bf
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-04-09 10:43:14 +0200

    Scaffolded out the main and grid components. Implemented the CSS necessary to complete the project.

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,43 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>
+        Grid
+      </div>
+    )
+  }
+}
+
+class Main extends React.Component {
+/**
+ *
+ */
+constructor() {
+  super();
+  this.state = {
+    generation: 0,
+  }
+}
+  render() {
+    return (
+      <div>
+        <h1>The Game of Life</h1>
+        <Grid
+        />
+        <h2>
+          Generations: {this.state.generation}
+        </h2>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main /> />
   </React.StrictMode>,
   document.getElementById('root')
 );
