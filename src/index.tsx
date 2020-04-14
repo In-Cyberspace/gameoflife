ffc044dc5ef9aeded8516a525a3f02c815190732
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-04-14 19:06:13 +0200

    Began constructing grid array.

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Grid extends React.Component {
+interface IGridProps {
+    gridFull: Array<any>,
+    
+    cols: number,
+    rows: number
+
+}
+
+class Grid extends Component<IGridProps> {
   render() {
+    const width = this.props.cols * 14;
+    var rowsArr:Array<any> = [];
+    var boxClass:string = "";
+
     return (
-      <div>
-        Grid
+      <div className="grid" style={{width: width}}>
+        {{rowsArr}}
       </div>
-    )
+    );
   }
 }
 
-class Main extends React.Component {
+interface IMainState {
+    generation: number,
+    gridFull: Array<any>
+}
+
+ interface IMainProps {
+    cols: number,
+    rows: number,
+    speed: number
+}
+
+class Main extends Component<IMainProps, IMainState> {
 /**
  *
  */
-constructor() {
-  super();
+constructor(props: IMainProps) {
+  super(props);
+
+  props = {
+      cols: 50,
+      rows: 30,
+      speed: 100
+  }
+
   this.state = {
     generation: 0,
+    gridFull: Array(this.props.rows)
+    .fill(0)
+    .map(() => Array(this.props.cols).fill(false))
   }
 }
+
   render() {
     return (
       <div>
         <h1>The Game of Life</h1>
         <Grid
+          gridFull={this.state.gridFull}
+          rows={this.props.rows}
+          cols={this.props.cols}
         />
         <h2>
           Generations: {this.state.generation}
         </h2>
       </div>
-    )
+    );
   }
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main /> />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
